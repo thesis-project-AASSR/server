97f110fde54d75869b148b08a15a51a64cfa87b4
AreenBadran <areenbadran9@gmail.com>
2020-12-16 23:53:04 +0200

    Backend-Deployed-Done

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,3 +4,6 @@ module.exports = {
   PASSWORD: "bd5ae418",
   DB: "heroku_a7840fa0418a5f2"
 };
+
+//to open global mysql type the following link on the terminal 
+// mysql --host=us-cdbr-east-02.cleardb.com --user=bd8e05add959b2 --password=bd5ae418 --reconnect heroku_a7840fa0418a5f2
