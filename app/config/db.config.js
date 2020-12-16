9a421b513b0e0acbceed3d8d7a364ed7b06722db
AreenBadran <areenbadran9@gmail.com>
2020-12-16 20:09:50 +0200

    second-commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "us-cdbr-east-02.cleardb.com",
-  USER: "b3d1878e080363",
-  PASSWORD: "104ecdbc",
-  DB: "heroku_5a7c957a8b3e661"
+  USER: "bd8e05add959b2",
+  PASSWORD: "bd5ae418",
+  DB: "heroku_a7840fa0418a5f2"
 };
