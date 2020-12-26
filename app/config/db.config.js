5785e8357d783e097374f64b6de50eeea543bda8
aboudaqrabawi <aqrabawijoe@gmail.com>
2020-12-26 14:43:10 +0200

    backend

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: '../../.env' });
-
 module.exports = {
-  HOST:'us-cdbr-east-02.cleardb.com',
-  USER: 'bd8e05add959b2',
-  PASSWORD: 'bd5ae418',
-  DB: 'heroku_a7840fa0418a5f2'
+  HOST: process.env.DATABASE_HOST,
+  USER: process.env.DATABASE_USER,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  DB: process.env.DATABASE
 };
-
 //to run server on heroku (globally)
 // heroku run node server.js
-
 //to open global mysql type the following link on the terminal 
 // mysql --host=us-cdbr-east-02.cleardb.com --user=bd8e05add959b2 --password=bd5ae418 --reconnect heroku_a7840fa0418a5f2
-
 //In postman
 // send the requests for https://backend-dawerha.herokuapp.com/ENDPOINT
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '../../.env' });
 var cors = require('cors')
 const app = express();
-
+const paypal = require('@paypal/payouts-sdk');
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -102,6 +102,71 @@ app.post("/signin", (req, res) => {
 
 require("./app/routes/routes.js")(app);
 // require("./app/routes/item.routes.js")(app2);
+
+
+
+let clientId = "Aes7p8OE4sBMwHolQa-6mkFoLsTZOpL-EIL8s26eW461Je9N9R5bcE9NzbsvuHADapydUlUZdT6mv19P";
+let clientSecret = "EOMfECpVcAbevWtpRnjzxhC4fMgnk39YT4rp4fV3V8CT8ZO11-2O56z1pAK_ZDSaAi910Eh12nj3L2_w";
+let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
+let client = new paypal.core.PayPalHttpClient(environment);
+
+app.post("/purchase", (req, res) => {
+    itemsInfo = {}
+  console.log("req.body:",req.body)
+ 
+  
+
+  console.log("itemsInfo:",itemsInfo);
+  var requestBody = {
+    "sender_batch_header": {
+      "recipient_type": "EMAIL",
+      "email_message": "SDK payouts test txn",
+      "note": "Enjoy your Payout!!",
+      // "sender_batch_id": itemsInfo.userId,
+      // "sender_batch_id":75,
+      "email_subject": "This is a test transaction from SDK"
+    }
+    ,
+    "items": [{
+      "note": "Your 5$ Payout!",
+      "amount": {
+        "currency": "USD",
+        // "value": 0.01
+        "value": req.body.price
+        // "value": itemsInfo.price
+      },
+      // "receiver": itemsInfo.receiver,
+      "receiver": "razan.tashman@yahoo.com",
+      "sender_item_id": req.body.sender_item_id
+      // "sender_item_id": 45
+    }]
+  }
+  // Construct a request object and set desired parameters 
+// Here, PayoutsPostRequest() creates a POST request to /v1/payments/payouts
+let request = new paypal.payouts.PayoutsPostRequest();
+request.requestBody(requestBody);
+
+// Call API with your client and get a response for your call
+  let createPayouts  = async function(){
+    let response = await client.execute(request);
+    console.log(`Response: ${JSON.stringify(response)}`);
+    // If call returns body in response, you can get the deserialized version from the result attribute of the response.
+   console.log(`Payouts Create Response: ${JSON.stringify(response.result)}`);
+   res.send(response)
+}
+createPayouts();
+})
+
+
+
+
+
+
+
+
+
+
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
